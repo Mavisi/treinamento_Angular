f62eab3fadb68583ce5a4924d97e58e1d8183d04
Marcos Santana <marcos.vssjr@outlook.com>
2025-04-22 17:01:20 -0300

    terminar em casa

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EnviaFormularioService } from '../../service/envia-formulario.service';
 
 @Component({
   selector: 'app-home',
@@ -8,17 +9,22 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  private enviaFormularioService= inject(EnviaFormularioService);
   name = 'Marcos';
   idButton =  "Marcos";
+  deveMostrarTitulo= true;
+  listItems = ["asda", "dois ", "tres"];
+
+  @Input() minhaPropsDeFora!: string;
+  
   //foi so um exemplo
 // meuBooleano = "Marcos";
-
 // atualizaBooleano(valor:any) {
 //   this.meuBooleano = valor;
 //   }
 
 submit() {
-  console.log("submit");
+  this.enviaFormularioService.enviarFormulario("oi");
 }
 }
+
